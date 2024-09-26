a510869aa751eab39dded2080a17f3ba459a847f
jrsiddhu <jrs.siddhu123@gmail.com>
2024-09-26 14:38:22 +0530

    op.c

---
+++ Week-4/op.c
@@ -0,0 +1,20 @@
+#include<stdto.h>
+int main () {
+ int a,b,c,d,e,f,g,s; 
+       printf("A=");
+         scanf("%d",&a);
+       printf("B="); 
+         scanf("%d",&b);
+       printf("C=");
+         scanf("%d",&c);
+       printf("D=");
+         scanf("%d",&d);
+       printf("E=");
+         scanf("%d",&e);
+       printf("F="); 
+         scanf("%d",&f); 
+       printf("G=");
+         scanf("%d",&g); 
+                s=a+b*c+(d*e)+f*g;
+              printf("A+B*C+(D*E)+F*G = %d\n",s);
+}
