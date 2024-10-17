c0d8630159eee54781dbb3e2dd6e74a65bcf2bb5
jrsiddhu <jrs.siddhu123@gmail.com>
2024-10-17 14:15:14 +0530

    MultiplicationTable.c

---
+++ Others/MultiplicationTable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   int i,n,p; 
+          printf("Enter Number = ");
+                scanf("%d",&i);
+                      for(n=1;n<=10;n++) {
+                                  p=i*n;     
+          printf("%d × %d = %d\n",i,n,p);}
+}
