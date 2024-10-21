e33561026032584523ced85f031c4721e412c50a
jrsiddhu <jrs.siddhu123@gmail.com>
2024-10-21 11:42:33 +0530

    Prime.c

---
+++ Others/Prime.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() { 
+  int n,i,f;
+  printf("Enter A Number = "); 
+  scanf("%d",&n); 
+  for(i=1;i<=n;i++){ 
+    if(n%i==0)
+      f++;}
+    if(f==2) 
+      printf("Prime Number");
+  else
+    printf("Not A Prime Number"); 
+}
